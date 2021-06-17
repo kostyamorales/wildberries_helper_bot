995d418ac3080d7236e8199d925a51d4b1cf9d9c
Konstantin Zemlyakov <kostyamorales@gmail.com>
2021-06-17 14:05:51 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,14 +5,15 @@ from validators import *
 class ValidatorsTestCase(TestCase):
 
     def test_validate_article(self):
-        self.assertEqual(validate_article('5904890'), None)
-        self.assertEqual(validate_article('9266068'), '9266068')
-        self.assertEqual(validate_article('?'), None)
+        self.assertEqual(validate_article('7777'), '7777')  # нет в наличии
+        self.assertEqual(validate_article('29242231'), '29242231')  # в наличии
+        self.assertEqual(validate_article('123'), None)  # артикул не существует
+        self.assertEqual(validate_article('?'), None)  # спец символы
 
     def test_validate_on_sale(self):
-        self.assertEqual(validate_item_price('5904890'), None)
-        self.assertEqual(validate_item_price('13668138'), 1835)
-        self.assertEqual(validate_item_price('?'), None)
+        # получаем уже проверенные артикулы после validate_article_tp
+        self.assertEqual(validate_article('7777'), None)  # нет в наличии
+        self.assertEqual(validate_article('29242231'), 1077)  # в наличии
 
     def test_validate_price(self):
         self.assertEqual(validate_price(1000, '900'), True)
